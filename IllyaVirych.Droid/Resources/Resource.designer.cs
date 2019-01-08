5bf27115296e71859ff43263a7c41388c082c530
virichhb5@gmail.com <Anuitex(Idea1)@DESKTOP-Q5AT7E4>
2019-01-08 10:09:04 +0200

    -menu

--- IllyaVirych.Droid/Resources/Resource.designer.cs
+++ IllyaVirych.Droid/Resources/Resource.designer.cs
@@ -6506,8 +6506,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090035
 			public const int CTRL = 2131296309;
 			
-			// aapt resource value: 0x7f0900bc
-			public const int Deletetask = 2131296444;
+			// aapt resource value: 0x7f0900bb
+			public const int Deletetask = 2131296443;
 			
 			// aapt resource value: 0x7f090036
 			public const int FUNCTION = 2131296310;
@@ -6521,8 +6521,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09001e
 			public const int MvxBindingTagUnique = 2131296286;
 			
-			// aapt resource value: 0x7f09009f
-			public const int RecyclerView = 2131296415;
+			// aapt resource value: 0x7f09009e
+			public const int RecyclerView = 2131296414;
 			
 			// aapt resource value: 0x7f090038
 			public const int SHIFT = 2131296312;
@@ -6530,11 +6530,11 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090039
 			public const int SYM = 2131296313;
 			
-			// aapt resource value: 0x7f0900bb
-			public const int Savetask = 2131296443;
+			// aapt resource value: 0x7f0900ba
+			public const int Savetask = 2131296442;
 			
-			// aapt resource value: 0x7f0900a7
-			public const int action0 = 2131296423;
+			// aapt resource value: 0x7f0900a6
+			public const int action0 = 2131296422;
 			
 			// aapt resource value: 0x7f09007e
 			public const int action_bar = 2131296382;
@@ -6557,17 +6557,17 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09005c
 			public const int action_bar_title = 2131296348;
 			
-			// aapt resource value: 0x7f0900a4
-			public const int action_container = 2131296420;
+			// aapt resource value: 0x7f0900a3
+			public const int action_container = 2131296419;
 			
 			// aapt resource value: 0x7f09007f
 			public const int action_context_bar = 2131296383;
 			
-			// aapt resource value: 0x7f0900ab
-			public const int action_divider = 2131296427;
+			// aapt resource value: 0x7f0900aa
+			public const int action_divider = 2131296426;
 			
-			// aapt resource value: 0x7f0900a5
-			public const int action_image = 2131296421;
+			// aapt resource value: 0x7f0900a4
+			public const int action_image = 2131296420;
 			
 			// aapt resource value: 0x7f090003
 			public const int action_menu_divider = 2131296259;
@@ -6584,11 +6584,11 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09005e
 			public const int action_mode_close_button = 2131296350;
 			
-			// aapt resource value: 0x7f0900a6
-			public const int action_text = 2131296422;
+			// aapt resource value: 0x7f0900a5
+			public const int action_text = 2131296421;
 			
-			// aapt resource value: 0x7f0900b4
-			public const int actions = 2131296436;
+			// aapt resource value: 0x7f0900b3
+			public const int actions = 2131296435;
 			
 			// aapt resource value: 0x7f09005f
 			public const int activity_chooser_view_content = 2131296351;
@@ -6605,8 +6605,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09003a
 			public const int always = 2131296314;
 			
-			// aapt resource value: 0x7f0900bd
-			public const int appbar = 2131296445;
+			// aapt resource value: 0x7f0900bc
+			public const int appbar = 2131296444;
 			
 			// aapt resource value: 0x7f090058
 			public const int async = 2131296344;
@@ -6626,8 +6626,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090065
 			public const int buttonPanel = 2131296357;
 			
-			// aapt resource value: 0x7f0900a8
-			public const int cancel_action = 2131296424;
+			// aapt resource value: 0x7f0900a7
+			public const int cancel_action = 2131296423;
 			
 			// aapt resource value: 0x7f090047
 			public const int center = 2131296327;
@@ -6641,8 +6641,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090075
 			public const int checkbox = 2131296373;
 			
-			// aapt resource value: 0x7f0900b0
-			public const int chronometer = 2131296432;
+			// aapt resource value: 0x7f0900af
+			public const int chronometer = 2131296431;
 			
 			// aapt resource value: 0x7f090050
 			public const int clip_horizontal = 2131296336;
@@ -6653,17 +6653,17 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09003b
 			public const int collapseActionView = 2131296315;
 			
-			// aapt resource value: 0x7f090091
-			public const int container = 2131296401;
+			// aapt resource value: 0x7f090090
+			public const int container = 2131296400;
 			
 			// aapt resource value: 0x7f090068
 			public const int contentPanel = 2131296360;
 			
-			// aapt resource value: 0x7f0900a1
-			public const int content_frame = 2131296417;
+			// aapt resource value: 0x7f0900a0
+			public const int content_frame = 2131296416;
 			
-			// aapt resource value: 0x7f090092
-			public const int coordinator = 2131296402;
+			// aapt resource value: 0x7f090091
+			public const int coordinator = 2131296401;
 			
 			// aapt resource value: 0x7f09006f
 			public const int custom = 2131296367;
@@ -6677,20 +6677,20 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090062
 			public const int default_activity_button = 2131296354;
 			
-			// aapt resource value: 0x7f090094
-			public const int design_bottom_sheet = 2131296404;
-			
-			// aapt resource value: 0x7f09009b
-			public const int design_menu_item_action_area = 2131296411;
+			// aapt resource value: 0x7f090093
+			public const int design_bottom_sheet = 2131296403;
 			
 			// aapt resource value: 0x7f09009a
-			public const int design_menu_item_action_area_stub = 2131296410;
+			public const int design_menu_item_action_area = 2131296410;
 			
 			// aapt resource value: 0x7f090099
-			public const int design_menu_item_text = 2131296409;
+			public const int design_menu_item_action_area_stub = 2131296409;
 			
 			// aapt resource value: 0x7f090098
-			public const int design_navigation_view = 2131296408;
+			public const int design_menu_item_text = 2131296408;
+			
+			// aapt resource value: 0x7f090097
+			public const int design_navigation_view = 2131296407;
 			
 			// aapt resource value: 0x7f090022
 			public const int disableHome = 2131296290;
@@ -6704,8 +6704,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090032
 			public const int end = 2131296306;
 			
-			// aapt resource value: 0x7f0900b6
-			public const int end_padder = 2131296438;
+			// aapt resource value: 0x7f0900b5
+			public const int end_padder = 2131296437;
 			
 			// aapt resource value: 0x7f090041
 			public const int enterAlways = 2131296321;
@@ -6749,8 +6749,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090064
 			public const int icon = 2131296356;
 			
-			// aapt resource value: 0x7f0900b5
-			public const int icon_group = 2131296437;
+			// aapt resource value: 0x7f0900b4
+			public const int icon_group = 2131296436;
 			
 			// aapt resource value: 0x7f09003c
 			public const int ifRoom = 2131296316;
@@ -6758,8 +6758,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090061
 			public const int image = 2131296353;
 			
-			// aapt resource value: 0x7f0900b1
-			public const int info = 2131296433;
+			// aapt resource value: 0x7f0900b0
+			public const int info = 2131296432;
 			
 			// aapt resource value: 0x7f09005b
 			public const int italic = 2131296347;
@@ -6767,11 +6767,11 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090000
 			public const int item_touch_helper_previous_elevation = 2131296256;
 			
-			// aapt resource value: 0x7f090090
-			public const int largeLabel = 2131296400;
+			// aapt resource value: 0x7f09008f
+			public const int largeLabel = 2131296399;
 			
-			// aapt resource value: 0x7f09009d
-			public const int layout_main = 2131296413;
+			// aapt resource value: 0x7f09009c
+			public const int layout_main = 2131296412;
 			
 			// aapt resource value: 0x7f09004b
 			public const int left = 2131296331;
@@ -6788,17 +6788,17 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090063
 			public const int list_item = 2131296355;
 			
-			// aapt resource value: 0x7f0900a0
-			public const int main_frame = 2131296416;
+			// aapt resource value: 0x7f09009f
+			public const int main_frame = 2131296415;
 			
-			// aapt resource value: 0x7f0900c3
-			public const int masked = 2131296451;
+			// aapt resource value: 0x7f0900c2
+			public const int masked = 2131296450;
 			
-			// aapt resource value: 0x7f0900aa
-			public const int media_actions = 2131296426;
+			// aapt resource value: 0x7f0900a9
+			public const int media_actions = 2131296425;
 			
-			// aapt resource value: 0x7f0900c1
-			public const int message = 2131296449;
+			// aapt resource value: 0x7f0900c0
+			public const int message = 2131296448;
 			
 			// aapt resource value: 0x7f090033
 			public const int middle = 2131296307;
@@ -6809,29 +6809,26 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09002a
 			public const int multiply = 2131296298;
 			
-			// aapt resource value: 0x7f0900b8
-			public const int name_text = 2131296440;
-			
-			// aapt resource value: 0x7f0900c6
-			public const int nav_about = 2131296454;
-			
-			// aapt resource value: 0x7f0900c4
-			public const int nav_items = 2131296452;
+			// aapt resource value: 0x7f0900b7
+			public const int name_text = 2131296439;
 			
 			// aapt resource value: 0x7f0900c5
-			public const int nav_task = 2131296453;
+			public const int nav_about = 2131296453;
 			
-			// aapt resource value: 0x7f09008e
-			public const int nav_view = 2131296398;
+			// aapt resource value: 0x7f0900c3
+			public const int nav_items = 2131296451;
+			
+			// aapt resource value: 0x7f0900c4
+			public const int nav_task = 2131296452;
+			
+			// aapt resource value: 0x7f0900a1
+			public const int navigation_frame = 2131296417;
+			
+			// aapt resource value: 0x7f090096
+			public const int navigation_header_container = 2131296406;
 			
 			// aapt resource value: 0x7f0900a2
-			public const int navigation_frame = 2131296418;
-			
-			// aapt resource value: 0x7f090097
-			public const int navigation_header_container = 2131296407;
-			
-			// aapt resource value: 0x7f0900a3
-			public const int navigation_menu_view = 2131296419;
+			public const int navigation_menu_view = 2131296418;
 			
 			// aapt resource value: 0x7f09003d
 			public const int never = 2131296317;
@@ -6842,14 +6839,14 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090020
 			public const int normal = 2131296288;
 			
-			// aapt resource value: 0x7f0900b3
-			public const int notification_background = 2131296435;
-			
-			// aapt resource value: 0x7f0900ad
-			public const int notification_main_column = 2131296429;
+			// aapt resource value: 0x7f0900b2
+			public const int notification_background = 2131296434;
 			
 			// aapt resource value: 0x7f0900ac
-			public const int notification_main_column_container = 2131296428;
+			public const int notification_main_column = 2131296428;
+			
+			// aapt resource value: 0x7f0900ab
+			public const int notification_main_column_container = 2131296427;
 			
 			// aapt resource value: 0x7f09004e
 			public const int parallax = 2131296334;
@@ -6875,11 +6872,11 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09004c
 			public const int right = 2131296332;
 			
-			// aapt resource value: 0x7f0900b2
-			public const int right_icon = 2131296434;
+			// aapt resource value: 0x7f0900b1
+			public const int right_icon = 2131296433;
 			
-			// aapt resource value: 0x7f0900ae
-			public const int right_side = 2131296430;
+			// aapt resource value: 0x7f0900ad
+			public const int right_side = 2131296429;
 			
 			// aapt resource value: 0x7f09000c
 			public const int save_image_matrix = 2131296268;
@@ -6953,14 +6950,14 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090027
 			public const int showTitle = 2131296295;
 			
-			// aapt resource value: 0x7f09008f
-			public const int smallLabel = 2131296399;
-			
-			// aapt resource value: 0x7f090096
-			public const int snackbar_action = 2131296406;
+			// aapt resource value: 0x7f09008e
+			public const int smallLabel = 2131296398;
 			
 			// aapt resource value: 0x7f090095
-			public const int snackbar_text = 2131296405;
+			public const int snackbar_action = 2131296405;
+			
+			// aapt resource value: 0x7f090094
+			public const int snackbar_text = 2131296404;
 			
 			// aapt resource value: 0x7f090045
 			public const int snap = 2131296325;
@@ -6983,8 +6980,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09004d
 			public const int start = 2131296333;
 			
-			// aapt resource value: 0x7f0900a9
-			public const int status_bar_latest_event_content = 2131296425;
+			// aapt resource value: 0x7f0900a8
+			public const int status_bar_latest_event_content = 2131296424;
 			
 			// aapt resource value: 0x7f090078
 			public const int submenuarrow = 2131296376;
@@ -6998,11 +6995,11 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090019
 			public const int tag_transition_group = 2131296281;
 			
-			// aapt resource value: 0x7f0900b9
-			public const int task_text = 2131296441;
+			// aapt resource value: 0x7f0900b8
+			public const int task_text = 2131296440;
 			
-			// aapt resource value: 0x7f0900b7
-			public const int test_layout = 2131296439;
+			// aapt resource value: 0x7f0900b6
+			public const int test_layout = 2131296438;
 			
 			// aapt resource value: 0x7f09001a
 			public const int text = 2131296282;
@@ -7016,8 +7013,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f09006b
 			public const int textSpacerNoTitle = 2131296363;
 			
-			// aapt resource value: 0x7f09009c
-			public const int text_input_password_toggle = 2131296412;
+			// aapt resource value: 0x7f09009b
+			public const int text_input_password_toggle = 2131296411;
 			
 			// aapt resource value: 0x7f090014
 			public const int textinput_counter = 2131296276;
@@ -7025,8 +7022,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090015
 			public const int textinput_error = 2131296277;
 			
-			// aapt resource value: 0x7f0900af
-			public const int time = 2131296431;
+			// aapt resource value: 0x7f0900ae
+			public const int time = 2131296430;
 			
 			// aapt resource value: 0x7f09001c
 			public const int title = 2131296284;
@@ -7037,17 +7034,17 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090071
 			public const int title_template = 2131296369;
 			
-			// aapt resource value: 0x7f0900ba
-			public const int togglebutton = 2131296442;
-			
-			// aapt resource value: 0x7f0900bf
-			public const int toolbar = 2131296447;
-			
-			// aapt resource value: 0x7f0900c0
-			public const int toolbar1 = 2131296448;
+			// aapt resource value: 0x7f0900b9
+			public const int togglebutton = 2131296441;
 			
 			// aapt resource value: 0x7f0900be
-			public const int toolbar2 = 2131296446;
+			public const int toolbar = 2131296446;
+			
+			// aapt resource value: 0x7f0900bf
+			public const int toolbar1 = 2131296447;
+			
+			// aapt resource value: 0x7f0900bd
+			public const int toolbar2 = 2131296445;
 			
 			// aapt resource value: 0x7f090040
 			public const int top = 2131296320;
@@ -7055,8 +7052,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090070
 			public const int topPanel = 2131296368;
 			
-			// aapt resource value: 0x7f090093
-			public const int touch_outside = 2131296403;
+			// aapt resource value: 0x7f090092
+			public const int touch_outside = 2131296402;
 			
 			// aapt resource value: 0x7f09000f
 			public const int transition_current_scene = 2131296271;
@@ -7073,8 +7070,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090013
 			public const int transition_transform = 2131296275;
 			
-			// aapt resource value: 0x7f09009e
-			public const int txt_name = 2131296414;
+			// aapt resource value: 0x7f09009d
+			public const int txt_name = 2131296413;
 			
 			// aapt resource value: 0x7f09002f
 			public const int uniform = 2131296303;
@@ -7088,8 +7085,8 @@ namespace IllyaVirych.Droid
 			// aapt resource value: 0x7f090016
 			public const int view_offset_helper = 2131296278;
 			
-			// aapt resource value: 0x7f0900c2
-			public const int visible = 2131296450;
+			// aapt resource value: 0x7f0900c1
+			public const int visible = 2131296449;
 			
 			// aapt resource value: 0x7f09003e
 			public const int withText = 2131296318;
--- IllyaVirych.Droid/ViewModels/AboutView.cs
+++ IllyaVirych.Droid/ViewModels/AboutView.cs
@@ -20,7 +20,7 @@ using Android.Support.V7.App;
 namespace IllyaVirych.Droid.ViewModels
 {
     [MvxFragmentPresentation(typeof(MainViewModel), Resource.Id.content_frame, true)]
-    public class AboutView : BaseFragment<AboutTaskViewModel>,NavigationView.IOnNavigationItemSelectedListener
+    public class AboutView : BaseFragment<AboutTaskViewModel>/*,NavigationView.IOnNavigationItemSelectedListener*/
     {
         protected override int FragmentId => Resource.Layout.AboutView;
 
@@ -28,24 +28,24 @@ namespace IllyaVirych.Droid.ViewModels
         {
             var view = base.OnCreateView(inflater, container, savedInstanceState);
             
-            DrawerLayout drawer = view.FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
-            var toolbar = view.FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar2);
-            ParentActivity.SetSupportActionBar(toolbar);
-            ParentActivity.SupportActionBar.SetDisplayHomeAsUpEnabled(true);
-            MvxActionBarDrawerToggle drawerToggle = new MvxActionBarDrawerToggle(
-            Activity, ((MainView)ParentActivity).DrawerLayout, toolbar, Resource.String.drawer_open, Resource.String.drawer_close);
-            drawerToggle.DrawerOpened += (object sender, ActionBarDrawerEventArgs e) => ((MainView)Activity)?.HideSoftKeyboard();
-            ((MainView)ParentActivity).DrawerLayout.AddDrawerListener(drawerToggle);            
-            drawerToggle.SyncState();
+            //DrawerLayout drawer = view.FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            //var toolbar = view.FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar2);
+            //ParentActivity.SetSupportActionBar(toolbar);
+            //ParentActivity.SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            //MvxActionBarDrawerToggle drawerToggle = new MvxActionBarDrawerToggle(
+            //Activity, ((MainView)ParentActivity).DrawerLayout, toolbar, Resource.String.drawer_open, Resource.String.drawer_close);
+            //drawerToggle.DrawerOpened += (object sender, ActionBarDrawerEventArgs e) => ((MainView)Activity)?.HideSoftKeyboard();
+            //((MainView)ParentActivity).DrawerLayout.AddDrawerListener(drawerToggle);            
+            //drawerToggle.SyncState();
 
-            NavigationView navigationView = view.FindViewById<NavigationView>(Resource.Id.nav_view);            
-            navigationView.SetNavigationItemSelectedListener(this);
+            //NavigationView navigationView = view.FindViewById<NavigationView>(Resource.Id.nav_view);            
+            //navigationView.SetNavigationItemSelectedListener(this);
             return view;
         }
 
-        public bool OnNavigationItemSelected(IMenuItem menuItem)
-        {
-            return true;
-        }
+        //public bool OnNavigationItemSelected(IMenuItem menuItem)
+        //{
+        //    return true;
+        //}
     }
 }
